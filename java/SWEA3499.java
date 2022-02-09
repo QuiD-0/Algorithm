4f11fe151c2da03324195f0ae8cf6eac24953a2d
QuiD <wodnd101@naver.com>
2022-02-09 10:57:08 +0900

    Update SWEA3499.java

--- java/SWEA3499.java
+++ java/SWEA3499.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class SWEA3499 {
@@ -9,23 +8,16 @@ public class SWEA3499 {
         for (int i = 1; i <= t; i++) {
             int n = Integer.parseInt(sc.nextLine());
             String[] cards = sc.nextLine().split(" ");
-            int a,b;
-            b=n/2;
-            a=n-b;
-            String[] order = new String[n];
-            for(int j=0;j<n;j++){
-                if(j%2==0){
-                    order[j]=cards[j/2];
-                }else{
-                    order[j]=cards[j/2+a];
+            int a = n - n / 2;
+            System.out.printf("#%d ", i);
+            for (int j = 0; j < n; j++) {
+                if (j % 2 == 0) {
+                    System.out.printf("%s ", cards[j / 2]);
+                } else {
+                    System.out.printf("%s ", cards[j / 2 + a]);
                 }
-            }
-            System.out.printf("#%d ",i);
-            for(String s:order){
-                System.out.printf("%s ",s);
             }
             System.out.println();
         }
-
     }
 }
