88e3fbf74e9f3e08c901975fc7cf6e12dde8b3c9
QuiD <wodnd101@naver.com>
2022-02-10 17:16:00 +0900

    Create SWEA1233.java

---
+++ java/SWEA1233.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class SWEA1233 {
+    public static int N;
+    public static String[] tree;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        for (int tc = 1; tc <= 10; ++tc) {
+            N = Integer.parseInt(br.readLine());
+
+            tree = new String[N + 1];
+            int answer = 1;
+
+            for (int i = 0; i < N; ++i) {
+                st = new StringTokenizer(br.readLine());
+                tree[Integer.parseInt(st.nextToken())] = st.nextToken();
+            }
+
+            int nodeIdx = N / 2;
+            for (int i = 1; i < N + 1; ++i) {
+                if (tree[i].charAt(0) >= '0' && tree[i].charAt(0) <= '9') {
+                    if (i <= nodeIdx) {
+                        answer = 0;
+                        break;
+                    }
+                }
+                else {
+                    if (i > nodeIdx) {
+                        answer = 0;
+                        break;
+                    }
+                }
+            }
+
+            sb.append("#").append(tc).append(" ").append(answer).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
