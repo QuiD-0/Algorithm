a7823959a135de402dc95a1c5b5963a059641101
QuiD <wodnd101@naver.com>
2022-02-21 13:09:42 +0900

    Create BOJ1759.java

---
+++ java/BOJ1759.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BOJ1759 {
+    static int l, c;
+    static String[] alpha;
+    static String[] ans;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        l = sc.nextInt();
+        c = sc.nextInt();
+        sc.nextLine();
+        alpha = sc.nextLine().split(" ");
+        Arrays.sort(alpha);
+        ans = new String[l];
+        find(0, 0);
+    }
+
+    private static void find(int cnt, int start) {
+        if (cnt >= l) {
+            int count=0;
+            int noCount=0;
+            for(String a :ans){
+                if(a.equals("a") || a.equals("e") || a.equals("i")|| a.equals("o")|| a.equals("u")){
+                    count++;
+                }else{
+                    noCount++;
+                }
+            }
+            if (count != 0 && noCount>=2) {
+                System.out.println(String.join("", ans));
+            }
+            return;
+        }
+        for(int i=start;i<c;i++){
+            ans[cnt]=alpha[i];
+            find(cnt+1,i+1);
+        }
+    }
+}
