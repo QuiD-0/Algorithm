19ae08e97e5e478461a0c1e88326eac245af98bf
QuiD-0 <wodnd101@naver.com>
2022-11-17 13:44:57 +0900

    update

--- 백준/1085직사각형에서 탈출.py
+++ 백준/1085직사각형에서 탈출.py
@@ -1,7 +1,11 @@
-a=list(map(int,input().split()))
-b=[]
-b.append(a[0])
-b.append(a[1])
-b.append(a[2]-a[0])
-b.append(a[3]-a[1])
-print(min(b))
+import heapq
+
+pos = list(map(int, input().split()))
+
+heap = []
+heapq.heappush(heap, pos[0])
+heapq.heappush(heap, pos[1])
+heapq.heappush(heap, pos[2] - pos[0])
+heapq.heappush(heap, pos[3] - pos[1])
+
+print(heapq.heappop(heap))
--- 백준/1654 랜선 자르기.py
+++ 백준/1654 랜선 자르기.py
@@ -1,16 +1,28 @@
-n,k=map(int,input().split())
-a=[]
+n, k = map(int, input().split())
+a = []
 for _ in range(n):
     a.append(int(input()))
-start, end = 1, max(a) 
+start, end = 1, max(a)
 
-while start <= end: 
-    mid = (start + end) // 2 
-    lines = 0 
+# while start <= end:
+#     mid = (start + end) // 2
+#     lines = 0
+#     for i in a:
+#         lines += i // mid
+#     if lines >= k:
+#         start = mid +1
+#     else:
+#         end = mid -1
+# print(end)
+#
+
+while start <= end:
+    mid = (start + end) // 2
+    lines = 0
     for i in a:
-        lines += i // mid 
-    if lines >= k: 
-        start = mid +1
+        lines += i // mid
+    if lines >= k:
+        start = mid + 1
     else:
-        end = mid -1
+        end = mid - 1
 print(end)
