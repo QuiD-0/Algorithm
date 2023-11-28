789e0aa9bfbdc6fbe435e23ee98169160ff92191
QuiD-0 <wodnd101@naver.com>
2023-11-28 22:54:13 +0900

    rename

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,8 +1,11 @@
 
 fun main() {
-    println(Solution())
+    println(Solution().solution(6, intArrayOf(1, 3, 2, 5, 4, 5, 2, 3)))
 }
 
 class Solution {
-
+    fun solution(k: Int, tangerine: IntArray): Int {
+        var answer: Int = 0
+        return answer
+    }
 }
