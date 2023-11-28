b71c0c986c66c6b2af5a8e3792067ea6b5ec9f07
QuiD-0 <wodnd101@naver.com>
2023-11-29 00:05:38 +0900

    solve

--- 프로그래머스/138476귤 고르기.kt
+++ 프로그래머스/138476귤 고르기.kt
@@ -1,8 +1,3 @@
-
-fun main() {
-    println(Solution().solution(4, intArrayOf(1, 3, 2, 5, 4, 5, 2, 3)))
-}
-
 class Solution {
     fun solution(k: Int, tangerine: IntArray): Int {
         val sortedBag = TangerineBag(tangerine).sortedBag
