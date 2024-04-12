f73dfb75aeef19da19b638a779fa3512f7c39852
QuiD-0 <wodnd101@naver.com>
2024-04-12 10:40:41 +0900

    solve - getHappyString

---
+++ leetCode/getHappyString.kt
@@ -0,0 +1,26 @@
+class Solution {
+    val arr = mutableListOf<String>()
+
+    fun getHappyString(n: Int, k: Int): String {
+        dfs(n,"")
+        println(arr)
+        return arr.getOrNull(k-1) ?: ""
+    }
+
+    private fun dfs(n: Int, word: String) {
+        if (n == 0) {
+            arr.add(word)
+            return
+        }
+
+        for(i in chars.indices){
+            if (word.isEmpty() || word[word.length - 1] != chars[i]) {
+                dfs( n-1, word+chars[i])
+            }
+        }
+    }
+
+    companion object {
+        val chars = arrayOf('a', 'b', 'c')
+    }
+}
