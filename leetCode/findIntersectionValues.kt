c40bc315fe5a93f96a9c561dbc3d5a9b499f1ae8
QuiD-0 <wodnd101@naver.com>
2024-04-26 10:17:13 +0900

    solve

---
+++ leetCode/findIntersectionValues.kt
@@ -0,0 +1,17 @@
+fun main() {
+    println(Solution().findIntersectionValues(intArrayOf(1, 2, 2, 1), intArrayOf(2, 2)))
+}
+
+class Solution {
+    fun findIntersectionValues(nums1: IntArray, nums2: IntArray): IntArray {
+        val arr1 = nums1.map {
+            if (nums2.contains(it)) 1 else 0
+        }.sumOf { it }
+
+        val arr2 = nums2.map {
+            if (nums1.contains(it)) 1 else 0
+        }.sumOf { it }
+
+        return intArrayOf(arr1, arr2)
+    }
+}
