bf86b6929824843e7341374cbbe5d19592ad1f99
QuiD-0 <wodnd101@naver.com>
2024-05-09 17:36:41 +0900

    solve

---
+++ leetCode/getLongestSubsequence.kt
@@ -0,0 +1,17 @@
+fun main() {
+    println(Solution())
+}
+
+class Solution {
+    fun getLongestSubsequence(words: Array<String>, groups: IntArray): List<String> {
+        val list: MutableList<String> = ArrayList()
+        var c = -1
+        for (i in words.indices) {
+            if (groups[i] !== c) {
+                c = groups[i]
+                list.add(words[i])
+            }
+        }
+        return list
+    }
+}
