6cf104d97ae39adc89d7f80dad5777cb856977d6
quid-0 <wodnd101@naver.com>
2024-11-26 11:09:02 +0900

    solve

---
+++ leetCode/productExceptSelf.kt
@@ -0,0 +1,21 @@
+fun main() {
+    Solution()
+        .productExceptSelf(intArrayOf(1, 2, 3, 4))
+        .also(::println)
+}
+
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val size = nums.size
+        val ans = IntArray(size) { 1 }
+        val right = IntArray(size) { 1 }
+        for (i in 1..<size) {
+            ans[i] = nums[i - 1] * ans[i - 1]
+        }
+        for (i in size - 2 downTo  0) {
+            right[i] = nums[i + 1] * right[i + 1]
+            ans[i] *= right[i]
+        }
+        return ans
+    }
+}
