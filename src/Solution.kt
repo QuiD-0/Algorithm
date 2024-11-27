0c1d987b3d1bf952b33373a519ecb1f22f6a695d
quid-0 <wodnd101@naver.com>
2024-11-27 23:43:07 +0900

    solve

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,39 +1,4 @@
 fun main() {
     Solution()
-        .maxArea(intArrayOf(1,8,6,2,5,4,8,3,7))
         .also(::println)
-}
-
-class Solution {
-    fun maxArea(height: IntArray): Int {
-        var (left, right) = 0 to height.size-1
-        var result = 0
-
-        while (left < right) {
-            val currentMax = min(height[left], height[right]) * (right - left)
-            result = max(result, currentMax)
-            if(height[left] > height[right]) {
-                right --
-            } else {
-                left ++
-            }
-        }
-        return result
-    }
-
-    private fun min(a:Int,b: Int): Int {
-        return if(a <= b) {
-            a
-        } else {
-            b
-        }
-    }
-
-    private fun max(a:Int, b:Int) :Int {
-        return if(a>=b) {
-            a
-        } else {
-            b
-        }
-    }
 }
