dcaee429127761541c0015e12a81050bb9abb601
quid-0 <wodnd101@naver.com>
2024-12-06 11:21:19 +0900

    solve

---
+++ leetCode/oddEvenList.kt
@@ -0,0 +1,28 @@
+fun main() {
+    Solution()
+        .also(::println)
+}
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class Solution {
+    fun oddEvenList(head: ListNode?): ListNode? {
+        var odd = head
+        var even = head?.next
+        val evenHead = even
+
+        while (even?.next != null) {
+            odd?.next = odd?.next?.next
+            even.next = even.next?.next
+
+            odd = odd?.next
+            even = even.next
+        }
+
+        odd?.next = evenHead
+
+        return head
+    }
+}
