1f9d349480e1801104dfdd69c5537363b8f79b99
quid-0 <wodnd101@naver.com>
2024-12-07 15:55:24 +0900

    solve

---
+++ leetCode/leafSimilar.kt
@@ -0,0 +1,30 @@
+fun main() {
+    Solution()
+        .also(::println)
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+class Solution {
+    fun leafSimilar(root1: TreeNode?, root2: TreeNode?): Boolean {
+        val leftNode = mutableListOf<Int>()
+        val rightNode = mutableListOf<Int>()
+
+        find(root1, leftNode)
+        find(root2, rightNode)
+
+        return leftNode == rightNode
+    }
+
+    private fun find(root: TreeNode?, list: MutableList<Int>) {
+        if (root?.left == null && root?.right == null) {
+            list.add(root!!.`val`)
+        } else {
+            find(root.left, list)
+            find(root.right, list)
+        }
+    }
+}
