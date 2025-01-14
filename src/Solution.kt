e561867b051d762537f987030d8519ad8ed072f7
quid-0 <wodnd101@naver.com>
2025-01-14 22:46:42 +0900

    solve

--- src/Solution.kt
+++ src/Solution.kt
@@ -2,3 +2,18 @@ fun main() {
     Solution()
         .also(::println)
 }
+
+class Solution {
+    fun tribonacci(n: Int): Int {
+        val dp = Array(n+1) { 0 }
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 1
+
+        for(i in 3 .. n) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        }
+
+        return dp[n]
+    }
+}
