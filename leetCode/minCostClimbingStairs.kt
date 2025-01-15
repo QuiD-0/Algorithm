b214ec0420e065f676c1faa051a676752ce30502
quid-0 <wodnd101@naver.com>
2025-01-15 20:33:32 +0900

    solve

---
+++ leetCode/minCostClimbingStairs.kt
@@ -0,0 +1,19 @@
+import kotlin.math.min
+
+fun main() {
+    Solution()
+        .minCostClimbingStairs(intArrayOf(10, 15, 20))
+        .also(::println)
+}
+
+class Solution {
+    fun minCostClimbingStairs(cost: IntArray): Int {
+        val n = cost.size
+        val dp = Array(n + 1) { 0 }
+
+        for (i in 2..n) {
+            dp[i] = min(cost[i - 1] + dp[i - 1], dp[i - 2] + cost[i - 2])
+        }
+        return dp[n]
+    }
+}
