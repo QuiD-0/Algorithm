eb130de949f33514f16753ba8a858cc899c6fdbd
quid-0 <wodnd101@naver.com>
2025-01-18 23:42:15 +0900

    solve

--- leetCode/singleNumber.kt
+++ leetCode/singleNumber.kt
@@ -4,15 +4,10 @@ fun main() {
 
 class Solution {
     fun singleNumber(nums: IntArray): Int {
-        val map = mutableMapOf<Int,Int>()
-        nums.forEach {
-            val orDefault = map.getOrDefault(it, 0)
-            map[it] = orDefault+1
+        var ans = 0
+        for(i in nums){
+            ans = ans xor i
         }
-        map.keys.forEach{
-            if (map[it] !=3)
-                return it
-        }
-        return 0
+        return ans
     }
 }
